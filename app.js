5b55a6150a53ff5b60813da888078b4f442861b6
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-16 19:53:31 -0400

    Add language
    Parsed ip too

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var express = require("express")
 var app = express()
 var useragent = require("express-useragent")
+var requestLanguage = require('express-request-language');
 
 app.set('port', process.env.PORT || 5000)
 app.use(express.static(__dirname + "/public"))
+
+app.use(requestLanguage({
+  languages: ['en-US']
+}));
 
 
 app.get("/api", (req, res) =>{
   var source = req.headers['user-agent']
   var ua = useragent.parse(source)
-  var obj = {"ipaddress": req.ip, "language":"hola", "software": ua.source}
+  var obj = {"ipaddress": req.ip.replace("::ffff:", ''), "language":req.language, "software": ua.source}
   res.writeHead(200, {"Content-Type":"application/json"})
   res.end(JSON.stringify(obj))
 })
